1328ca191f9f74a567aa2a8aceb4d91c4688993a
Anna <anna_agabekyan_anna@mail.ru>
2020-02-23 14:56:13 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,12 +1,28 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array !== undefined && array.length !== 0) {
+    return Math.min.apply(null, array);
+} else {
+    return 0;
+}
 }
 
 exports.max = function max (array) {
+  if (array && array.length > 0){
+    return array.reduce((max, current) => max = max < current ? current : max, -Infinity);
+  }
   return 0;
 }
 
+
 exports.avg = function avg (array) {
-  return 0;
-}
+  let sum = 0;
+    if (Array.isArray(array) && array.length > 0) {
+      for (let i = 0; i < array.length; i++) {
+        sum += array[i];
+      }
+      return sum / array.length;
+    }
+    return 0;
+  }
+
